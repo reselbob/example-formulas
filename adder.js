211353a1703ce77e11e61d49d66cabfe9450212d
Bob Reselman <bob@CogArtTech.com>
2022-09-14 15:37:26 -0700

    Initial checkin

---
+++ adder.js
@@ -0,0 +1,7 @@
+// take input from the user
+const A = parseInt(prompt("Enter a positive number: "));
+const B = parseInt(prompt("Enter another positive number: "));
+
+const C = A + B;
+
+console.log(`The sum of ${A} + ${B} is ${C}`)
