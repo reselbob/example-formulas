7ca84b5d5c26c51587474f6cee8de21e67320190
Bob Reselman <bob@CogArtTech.com>
2022-09-14 19:34:00 -0700

    Create find-all-primes.js

---
+++ find-all-primes.js
@@ -0,0 +1,22 @@
+function isPrime(num) {
+    for (let i = 2; i * i <= num; i++)
+        if (num % i === 0)
+            return false;
+    return num > 1;
+}
+
+// take input from the user
+const limit = parseInt(prompt("Enter the numeric limit under which to find all prime numbers: "));
+const finds = [];
+
+
+for (let i =3; i <=limit; i++){
+    if(isPrime(i)){
+      finds.push(i)
+    }
+    i++;
+}
+console.log(`Here are all the prime numbers under the ${limit}:`)
+finds.forEach(f =>{
+  console.log(f);
+})
